24f77b51890138b04d8cfd10e17213ed58883b04
Georges Kanaan <georgeskanan@gmail.com>
2024-05-30 08:59:38 +0200

    first

---
+++ src/main/java/org/librairie/Main.java
@@ -0,0 +1,15 @@
+package org.librairie;
+
+import org.librairie.controller.EcrivainController;
+import org.librairie.model.entity.Ecrivain;
+
+public class Main {
+    public static void main(String[] args) {
+        EcrivainController ec = new EcrivainController();
+        ec.afficheEcrivains();
+//        ec.create(new Ecrivain("Pinker","Steve"));
+//        ec.create(new Ecrivain("Smith","Jacke"));
+// créer la vue pour saisir le Scanner.
+        // ec.modifNom(4,"Fondax");
+    }
+}
